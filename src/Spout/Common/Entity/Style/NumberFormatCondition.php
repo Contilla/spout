db7ea7a53a05005f7e16f950f419d363df0d8876
Oliver Friedrich <friedrich@contilla.de>
2019-04-03 11:28:41 +0200

    Conditions for number formats

---
+++ src/Spout/Common/Entity/Style/NumberFormatCondition.php
@@ -0,0 +1,113 @@
+<?php
+
+/**
+ * NumberFormatCondition
+ * 
+ * Description of NumberFormatCondition
+ *
+ * @copyright (c) Expression year is undefined on line 7, column 21 in Templates/Scripting/PHPClass.php., Contilla GmbH
+ * @author Oliver Friedrich <friedrich@contilla.de>
+ * @version 1.0, 11.03.2019
+ */
+
+namespace Box\Spout\Common\Entity\Style;
+
+/**
+ * NumberFormatCondition
+ * 
+ * Description of NumberFormatCondition
+ *
+ * @copyright (c) Expression year is undefined on line 21, column 21 in Templates/Scripting/PHPClass.php., Contilla GmbH
+ * @author Oliver Friedrich <friedrich@contilla.de>
+ * @version 1.0, 11.03.2019
+ */
+class NumberFormatCondition {
+
+    private $value;
+    private $comparator;
+
+    /**
+     * Compared number must be lower than the conditions value
+     */
+    const COMPARE_LOWERTHAN = 0;
+
+    /**
+     * Compared number must be lower or equal to the conditions value
+     */
+    const COMPARE_LOWEREQUAL = 1;
+
+    /**
+     * Compared number must be equal to the conditions value
+     */
+    const COMPARE_EQUAL = 2;
+
+    /**
+     * Compared number must be greater or equal to the conditions value
+     */
+    const COMPARE_GREATEREQUAL = 3;
+
+    /**
+     * Compared number must be greater than the conditions value
+     */
+    const COMPARE_GREATERTHAN = 4;
+
+    private static $availableComparators = [self::COMPARE_EQUAL, self::COMPARE_GREATEREQUAL, self::COMPARE_GREATERTHAN, self::COMPARE_LOWEREQUAL, self::COMPARE_LOWERTHAN];
+
+    public function __construct($value, $comparator) {
+        if (!is_numeric($value)) {
+            throw new \Exception('Value must be numeric');
+        }
+
+        if (!in_array($comparator, self::$availableComparators)) {
+            throw new \Exception('Comparator must be one of the COMPARE_XX class constants');
+        }
+
+        $this->value = $value;
+        $this->comparator = $comparator;
+    }
+
+    /**
+     * Get the value to compare against
+     * 
+     * @return numeric
+     */
+    public function getValue() {
+        return $this->value;
+    }
+
+    /**
+     * Get the comparator of the condition
+     * 
+     * @see NumberFormatCondition::COMPARE_XX constants
+     * @return int 
+     */
+    public function getComparator() {
+        return $this->comparator;
+    }
+
+    public static function getComparatorType($comparatorString) {
+        switch (trim($comparatorString)) {
+            case '<':
+                $type = self::COMPARE_LOWERTHAN;
+                break;
+            case '<=':
+                $type = self::COMPARE_LOWEREQUAL;
+                break;
+            case '=':
+                $type = self::COMPARE_EQUAL;
+                break;
+            case '>=':
+                $type = self::COMPARE_GREATEREQUAL;
+                break;
+            case '>':
+                $type = self::COMPARE_GREATERTHAN;
+                break;
+            default:
+                throw new \Exception('Unknown comparator string');
+                break;
+        }
+
+        return $type;
+    }
+
+}
