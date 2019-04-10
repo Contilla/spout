e02043d3b39fafbabac12c44ea4f940b6cd0692b
Oliver Friedrich <friedrich@contilla.de>
2019-04-10 16:24:28 +0200

    Extending NumberstyleBuilder Add phpdoc

--- src/Spout/Writer/Common/Creator/Style/NumberStyleBuilder.php
+++ src/Spout/Writer/Common/Creator/Style/NumberStyleBuilder.php
@@ -3,7 +3,8 @@
 /**
  * NumberformatBuilder
  * 
- * Builder to create new number format definitions
+ * Builder to create new number format definitions. More or less this is a dummy class as all useful functionality is bundeled in \Box\Spout\Common\Entity\Style\NumberStyle
+ * @see \Box\Spout\Common\Entity\Style\NumberStyle
  *
  * @copyright (c) 2019, Contilla GmbH
  * @author Oliver Friedrich <friedrich@contilla.de>
@@ -15,32 +16,41 @@ namespace Box\Spout\Writer\Common\Creator\Style;
 /**
  * NumberformatBuilder
  * 
- * Builder to create new number format definitions
+ * Builder to create new number format definitions. More or less this is a dummy class as all useful functionality is bundeled in \Box\Spout\Common\Entity\Style\NumberStyle
+ * @see \Box\Spout\Common\Entity\Style\NumberStyle
  *
  * @copyright (c) 2019, Contilla GmbH
  * @author Oliver Friedrich <friedrich@contilla.de>
  * @version 1.0, 01.02.2019
  */
-class NumberformatBuilder {
+class NumberStyleBuilder {
     
     /**
-     * @var \Box\Spout\Common\Entity\Style\NumberFormat
+     * @var \Box\Spout\Common\Entity\Style\NumberStyle
      */
-    protected $numberFormat=null;
+    protected $numberStyle=null;
     
     public function __construct()
     {
-        $this->numberFormat = new \Box\Spout\Common\Entity\Style\NumberFormat();
+        $this->numberStyle = new \Box\Spout\Common\Entity\Style\NumberStyle();
     }
     
     /**
      * Set the format code
      * 
+     * This will overwrite the internal numberstyle by the new definition
+     * 
      * @param string $formatCode
      * @return $this
      */
     public function setFormatCode($formatCode) {
-        $this->numberFormat->setFormatCode($formatCode);
+        $this->numberStyle = \Box\Spout\Common\Entity\Style\NumberStyle::build($formatCode);
+        
+        return $this;
+    }
+    
+    public function setColor($color) {
+        $this->numberStyle->setColor($color);
         
         return $this;
     }
@@ -48,13 +58,13 @@ class NumberformatBuilder {
     /**
      * Get the format definition
      * 
-     * @return \Box\Spout\Common\Entity\Style\NumberFormat
+     * @return \Box\Spout\Common\Entity\Style\NumberStyle
      */
     public function build() {
-        return $this->numberFormat;
+        return $this->numberStyle;
     }
     
     public function getFormat() {
-        return $this->numberFormat;
+        return $this->numberStyle;
     }
 }
